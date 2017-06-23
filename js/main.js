46b27e28c6f518e9469a06348729e9a1520f6d40
Tiffany Monroe <tiffanykmonroe@gmail.com>
2017-06-23 14:55:41 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -21,17 +21,28 @@ var cards = [
 ];
   var cardsInPlay = [];
   var checkForMatch = function (){
+    document.getElementsByTagName('cardImage')[0].setAttribute('src');
     if (cardsInPlay.length === 2) {
       if (cardsInPlay[0] === cardsInPlay[1]) alert('You found a match!');
       if (cardsInPlay[0] != cardsInPlay[1]) alert('Sorry, try again.');
     }
   }
-  var flipCard = function (cardId) {
+  var flipCard = function () {
+    this.getAttribute('data-id');
     console.log("User flipped " + cards[cardId].rank)
     cardsInPlay.push(cards[cardId].rank)
     console.log(cards[cardId].cardImage)
     console.log(cards[cardId].suit)
   }
 
-  flipCard(0);
-  flipCard(2);
+  var createBoard = function () {
+    for (var i = 0; i < cards.length; i += 1) {
+      var cardElement = document.createElement('img', "images/back.png");
+      cardElement.setAttribute('data-id', i);
+      cardElement.addEventListener('click', flipCard);
+      document.getElementsByTagName('game-board')[0].appendChild(cards);
+      gameBoard();
+    }
+  }
+
+createBoard();
