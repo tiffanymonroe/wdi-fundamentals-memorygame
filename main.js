f5f5d624e496f6347a8dd9461d7f443c861f875b
Tiffany Monroe <tiffanykmonroe@gmail.com>
2017-06-23 01:32:39 -0700

    created game logic

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
  console.log("Up and running!");
- 
- var cardOne = "queen";
- var cardTwo = "king";
- var cardThree = "queen";
- var cardFour = "king";
 
- console.log("User flipped " + cardOne);
- console.log("User flipped " + cardFour);
- 
+  var cards = ["queen", "queen", "king", "king"];
+  var cardsInPlay = [];
+  var cardOne = cards[0];
+  cardsInPlay.push(cardOne);
+  cardsInPlay;
+  var cardTwo = cards[2];
+  cardsInPlay.push(cardTwo);
+  cardsInPlay;
 
+  console.log("User flipped " + cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) alert('You found a match!');
+  if (cardsInPlay[0] != cardsInPlay[1]) alert('Sorry, try again.');
+}
